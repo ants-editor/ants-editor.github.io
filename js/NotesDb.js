4edf597ab599b0acfd9a2fe54f78d5f4da6219dd
Nextor Leon <nextor@gmail.com>
2019-05-06 09:40:26 -0700

    Upgrading

--- js/NotesDb.js
+++ js/NotesDb.js
@@ -7,7 +7,7 @@ export default class NoteDb
 {
 	constructor()
 	{
-		this.database	= Finger.builder('notes',20,{
+		this.database	= Finger.builder('notes',21,{
 			'note':'id,title,search,*tags,updated,access_count',
 			'note_terms':'++id,note_id,term',
 			'backup':'id'
